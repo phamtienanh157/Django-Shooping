993c6317c8e84ab3d3ec8040b982eac419a1d6ee
phamtienanh157 <phamtienanh157@gmail.com>
2022-03-23 22:10:25 +0700

    ui item details

--- home/views.py
+++ home/views.py
@@ -1,16 +1,16 @@
 from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Book, Store
+from .models import Book, BookItem
 # Create your views here.
 
 
 def getListBooks(request):
-    data = {'Books': Book.objects.all()}
+    data = {'Books': BookItem.objects.all()}
     return render(request, 'pages/home.html', data)
 
 
 def getBookById(request, id):
-    book = Book.objects.get(id=id)
-    data = {'book': book}
+    book = BookItem.objects.get(id=id)
+    data = {'bookItem': book}
     return render(request, 'pages/itemDetails.html', data)
