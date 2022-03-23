81f3bae238232109e41ec6b8ef16ef5dbedbc1c9
phamtienanh157 <phamtienanh157@gmail.com>
2022-03-23 20:31:51 +0700

    shopping cart

--- home/models.py
+++ home/models.py
@@ -5,13 +5,6 @@ from django.db import models
 # Create your models here.
 
 
-class Store(models.Model):
-    name = models.CharField(max_length=50)
-    address = models.CharField(max_length=50)
-    city = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-
-
 class Author(models.Model):
     name = models.CharField(max_length=50)
 
