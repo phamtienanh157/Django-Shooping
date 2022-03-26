42c82880c9d27eb4f963a6fb07238c1ac240b683
phamtienanh157 <phamtienanh157@gmail.com>
2022-03-26 08:35:18 +0700

    add more book

--- home/models.py
+++ home/models.py
@@ -8,14 +8,23 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Publisher(models.Model):
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Book(models.Model):
@@ -26,6 +35,9 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.title}"
+
 
 class BookItem(models.Model):
     book = models.OneToOneField(
@@ -33,3 +45,6 @@ class BookItem(models.Model):
     price = models.FloatField(default=0)
     discount = models.FloatField(default=0)
     image = models.URLField(default='')
+
+    def __str__(self):
+        return f"{self.book.title}"
