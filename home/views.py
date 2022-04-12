a3e168935f4c4408fcc1bb2544dec543c3f48a8c
phamtienanh157 <phamtienanh157@gmail.com>
2022-04-12 20:45:02 +0700

    first

--- home/views.py
+++ home/views.py
@@ -5,12 +5,5 @@ from .models import Book, BookItem
 # Create your views here.
 
 
-def getListBooks(request):
-    data = {'Books': BookItem.objects.all()}
-    return render(request, 'pages/home.html', data)
-
-
-def getBookById(request, id):
-    book = BookItem.objects.get(id=id)
-    data = {'bookItem': book}
-    return render(request, 'pages/itemDetails.html', data)
+def renderHome(request):
+    return render(request, 'pages/home.html')
